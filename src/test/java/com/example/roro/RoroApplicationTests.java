002404cd158c023315b2964885af3a4d6abd149e
Roro0630 <ksk6736@naver.com>
2025-01-10 11:24:27 +0900

    첫 커밋

---
+++ src/test/java/com/example/roro/RoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
